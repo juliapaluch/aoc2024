c3a6afdfb9d4c2ea5e237af2fa4536332233d88a
Julia Paluch <paluchj4@gmail.com>
2024-12-03 02:03:21 -0500

    day 3 part 2

--- day03/rust/src/main.rs
+++ day03/rust/src/main.rs
@@ -5,8 +5,25 @@ use std::io::{self, prelude::*, BufReader};
 fn part_one(file: &File) -> i64 {
     let reader = BufReader::new(file);
     let line = reader.lines().nth(0).unwrap().unwrap();
-    let re = Regex::new(r"mul\((?<l>\d{1,3}),(?<r>\d{1,3})\)").unwrap();
-    re.captures_iter(&line)
+    Regex::new(r"mul\((?<l>\d{1,3}),(?<r>\d{1,3})\)")
+        .unwrap()
+        .captures_iter(&line)
+        .map(|c| {
+            c.name("l").unwrap().as_str().parse::<i64>().unwrap()
+                * c.name("r").unwrap().as_str().parse::<i64>().unwrap()
+        })
+        .sum()
+}
+
+fn part_two(file: &File) -> i64 {
+    let reader = BufReader::new(file);
+    let line = reader.lines().nth(0).unwrap().unwrap();
+    let new_line = Regex::new(r"don't\(\).*?do\(\)")
+        .unwrap()
+        .replace_all(&line, "");
+    Regex::new(r"mul\((?<l>\d{1,3}),(?<r>\d{1,3})\)")
+        .unwrap()
+        .captures_iter(&new_line)
         .map(|c| {
             c.name("l").unwrap().as_str().parse::<i64>().unwrap()
                 * c.name("r").unwrap().as_str().parse::<i64>().unwrap()
@@ -17,7 +34,7 @@ fn part_one(file: &File) -> i64 {
 fn main() -> io::Result<()> {
     let mut file = File::open("src/input.txt")?;
     println!("Part 1: {}", part_one(&file));
-    // file.rewind()?;
-    // println!("Part 2: {}", part_two(&file));
+    file.rewind()?;
+    println!("Part 2: {}", part_two(&file));
     Ok(())
 }
