a793809b1349ee8f8962fcb9a0fa87cf9c63a3c6
Julia Paluch <paluchj4@gmail.com>
2024-12-09 03:42:12 -0500

    day 8 part 2

--- day08/rust/src/main.rs
+++ day08/rust/src/main.rs
@@ -77,6 +77,73 @@ fn part_one(file: &File) -> usize {
     antinodes.len()
 }
 
+fn part_two(file: &File) -> usize {
+    let reader: BufReader<&File> = BufReader::new(file);
+    let antennas: Vec<Antenna> = reader
+        .lines()
+        .enumerate()
+        .map(|(y, row)| {
+            row.unwrap()
+                .chars()
+                .enumerate()
+                .filter_map(move |(x, c)| match c {
+                    '.' => None,
+                    _ => Some(Antenna {
+                        location: Point {
+                            x: x as i16,
+                            y: y as i16,
+                        },
+                        frequency: c,
+                    }),
+                })
+                .collect::<Vec<_>>()
+        })
+        .flatten()
+        .collect::<Vec<Antenna>>();
+
+    let mut antinodes: HashSet<Point> = HashSet::<Point>::new();
+
+    antennas.iter().for_each(|current_antenna: &Antenna| {
+        antennas
+            .iter()
+            .filter(|other_antenna| {
+                other_antenna.frequency == current_antenna.frequency
+                    && other_antenna.location != current_antenna.location
+            })
+            .for_each(|other_antenna: &Antenna| {
+                antinodes.insert(current_antenna.location);
+                antinodes.insert(other_antenna.location);
+                let diff = Point {
+                    x: other_antenna.location.x - current_antenna.location.x,
+                    y: other_antenna.location.y - current_antenna.location.y,
+                };
+                let mut next = Point {
+                    x: other_antenna.location.x + diff.x,
+                    y: other_antenna.location.y + diff.y,
+                };
+                while valid(next) {
+                    antinodes.insert(next);
+                    next = Point {
+                        x: next.x + diff.x,
+                        y: next.y + diff.y,
+                    };
+                }
+                let mut previous = Point {
+                    x: current_antenna.location.x + (-1 * diff.x),
+                    y: current_antenna.location.y + (-1 * diff.y),
+                };
+                while valid(previous) {
+                    antinodes.insert(previous);
+                    previous = Point {
+                        x: previous.x + (-1 * diff.x),
+                        y: previous.y + (-1 * diff.y),
+                    };
+                }
+            })
+    });
+    antinodes.len()
+}
+
 fn valid(p: Point) -> bool {
     p.x >= 0 && p.x <= MAX_X && p.y >= 0 && p.y <= MAX_Y
 }
@@ -86,7 +153,7 @@ fn main() -> io::Result<()> {
     let p1 = part_one(&file);
     println!("Part 1: {}", p1);
     file.rewind()?;
-    // let p2 = part_two(&file);
-    // println!("Part 2: {}", p2);
+    let p2 = part_two(&file);
+    println!("Part 2: {}", p2);
     Ok(())
 }
